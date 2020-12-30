0af86fee33a073250a48f84fd4010f309a09ec42
AdamMcWilliam <adammcwilliam@gmail.com>
2020-12-29 18:27:52 -0600

    clean up

--- assets/js/imgurUploader.js
+++ assets/js/imgurUploader.js
@@ -2,7 +2,6 @@ function uploadImg(imgToUpload) {
     // Begin file upload
     console.log("Uploading file to Imgur..");
 
-    // Replace ctrlq with your own API key
     var apiUrl = 'https://api.imgur.com/3/image';
     var apiKey = '0356f753b33adbb';
 
