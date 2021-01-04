f223f915b2a1a4fa9c59c98099a724cfd862e3a3
Adam <adammcwilliam@gmail.com>
2021-01-04 12:59:11 -0600

    Add slide in bar for uploaded images

--- assets/js/index.js
+++ assets/js/index.js
@@ -255,3 +255,14 @@
     document.getElementById("pipeBtn").addEventListener("click", function() {
         copyDivToClipboard('pipedCommands');
     });
+
+
+    /* Set the width of the side navigation to 250px */
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "300px";
+    }
+
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
